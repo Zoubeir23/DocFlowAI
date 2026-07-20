602f8b8a1a7e5fc5a9883724f15691bd1e061b35
zoubeir23 <zoubeiribrahima@gmail.com>
2026-07-20 22:20:28 +0000

    fix: sendDefaultPii=false + scrubbing sur la config Sentry client

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -24,9 +24,11 @@ Sentry.init({
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
 
-  // Enable sending user PII (Personally Identifiable Information)
+  // sendDefaultPii désactivé : application médicale, pas d'IP/cookies par défaut.
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
+
+  beforeSend: (event) => scrubSentryEvent(event),
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
